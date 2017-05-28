0d907a1aa05e0009bfa5894821e1e65981328dfe
Abhi Nayar <abhishek.nayar@yale.edu>
2017-05-28 04:08:26 -0400

    fixed favicon and icon console errors

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.set('port', process.env.PORT || 8080)
 app.set('view engine', 'hbs')
 app.set('views', path.join(__dirname, '/views'))
 app.use(express.static(__dirname + '/static/'))
-app.use(favicon(path.join(__dirname, 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'favicon.ico')))
 
 app.get('/', function(req, res) {
   res.redirect('/comingSoon');
@@ -17,14 +17,14 @@ app.get('/', function(req, res) {
 app.get('/defaultsite', function(req, res) {
   res.redirect('/comingSoon');
 })
-app.get('/comingSoon', function(req, res) {  
+app.get('/comingSoon', function(req, res) {
   //EDIT THIS FILE
   var landingContent = require(__dirname + '/dynamicContent/landingPage.json');
   res.render('landing', {
     content : landingContent
   });
 })
-        
+
 app.listen(app.get('port'), function(){
   console.log('tonebase app listenting on port ' + app.get('port'));
 })
